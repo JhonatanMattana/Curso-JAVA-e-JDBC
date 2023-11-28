f4758812ed1775691f8d3ce5e2670d7e85202d86
Jhonatan Mattana <kaverinhaa@hotmail.com>
2023-11-27 23:01:40 -0300

    Aula: Usando o try-with-resources

--- src/main/java/br/curso/java/jdbc/TestaInsercaoComParametro.java
+++ src/main/java/br/curso/java/jdbc/TestaInsercaoComParametro.java
@@ -10,28 +10,25 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao();
 		
-		connection.setAutoCommit(false);
-		
-		try {
-			PreparedStatement prepareStatement = connection.prepareStatement(" "
-					+ "INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?) ", 
-					Statement.RETURN_GENERATED_KEYS);
+		try ( Connection connection = connectionFactory.recuperarConexao()) {
+			connection.setAutoCommit(false);
 			
-			adicionarVariavel("SmartTV", "45 Polegadas", prepareStatement);
-			adicionarVariavel("Radio", "Radio de bateria", prepareStatement);
-			
-			connection.commit();
-			
-			prepareStatement.close();
-			connection.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
+			try (
+					PreparedStatement prepareStatement = connection.prepareStatement(" "
+							+ "INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?) ", 
+							Statement.RETURN_GENERATED_KEYS);
+					) {
+				adicionarVariavel("SmartTV", "45 Polegadas", prepareStatement);
+				adicionarVariavel("Radio", "Radio de bateria", prepareStatement);
+				
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
-		
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement prepareStatement) throws SQLException {
@@ -39,12 +36,11 @@ public class TestaInsercaoComParametro {
 		prepareStatement.setString(2, descricao);
 		
 		prepareStatement.execute();
-		
-		ResultSet generatedKeys =  prepareStatement.getGeneratedKeys();
-		
-		while (generatedKeys.next()) {
-			Integer id = generatedKeys.getInt(1);
-			System.out.println("O ID criado foi: " + id);
+		try ( ResultSet generatedKeys =  prepareStatement.getGeneratedKeys(); ) {
+			while (generatedKeys.next()) {
+				Integer id = generatedKeys.getInt(1);
+				System.out.println("O ID criado foi: " + id);
+			}
 		}
 	}
 }
