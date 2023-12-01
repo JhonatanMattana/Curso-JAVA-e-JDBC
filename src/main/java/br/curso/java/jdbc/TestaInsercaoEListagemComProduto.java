8af514eed790782495526004d79896633809368d
Jhonatan Mattana <kaverinhaa@hotmail.com>
2023-11-30 21:47:52 -0300

    Aula: Listando no ProdutoDAO

--- src/main/java/br/curso/java/jdbc/TestaInsercaoEListagemComProduto.java
+++ src/main/java/br/curso/java/jdbc/TestaInsercaoEListagemComProduto.java
@@ -2,11 +2,12 @@ package br.curso.java.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import br.curso.java.jdbc.dao.ProdutoDAO;
 import br.curso.java.jdbc.modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
 
@@ -15,6 +16,9 @@ public class TestaInsercaoComProduto {
 		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
 			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
 			produtoDAO.salvar(comoda);
+			
+			List<Produto> listaDeProdutos = produtoDAO.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
 	}
 
